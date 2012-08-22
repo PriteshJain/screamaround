4251f72c4db2f4440aa8ac4f56b986ee1b8cd2e5
Pritesh Jain <prit.jain86@gmail.com>
2012-08-22 23:52:22 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScreamAround::Application.config.secret_token = '8cbe717e0f14962c3f6b985f37afb747977b30002b5d1feefc74a0733e7c2ce9899cb7f6234dad29fa0fbcb31a0bdc25cdd2a3c6573cab8b36ba52a36f7e9df9'
